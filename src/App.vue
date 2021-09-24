d9109ac01bca5f370f8d8fb696ca9d0cbafc2594
Aldemar0118 <79219065+Aldemar0118@users.noreply.github.com>
2021-09-23 20:15:14 -0500

    login

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,136 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+ <table class="container">
+        <tr>
+            <td class="navbar" >
+            <img src="./assets/Logo_blanco.png" class="logo" alt="Inicio" >
+
+        <ul>
+            
+            <li><a href="quienes_somos.html">Quienes Somos</a></li>
+            <li><router-link to="/login" class="btn-Inicio-index">Inicio de Secion</router-link></li>
+        </ul>
+    </td>
+    </tr>
+</table>
+<br>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+*{
+   margin: 0;
+   padding: 0;
+}
+li{
+   padding: 5px 2px 2px 4px;
+}
+.container {
+   width: 95%;
+   height:14vh;
+   background-image: linear-gradient(rgba(5, 10, 5, 0.80), rgba(42,93,103));
+   background-size: cover;
+   background-position: center;
+   background-repeat: no-repeat;
 }
 
-#nav {
-  padding: 30px;
+.navbar {
+   width: 100%;
+   margin: auto;
+   padding: 35px 0;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.logo {
+   width: 130px;
+   cursor: pointer;
+   padding: 0px 10px 0px 10px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.navbar ul li {
+   list-style: none;
+   display: inline-block;
+   margin: 0 20px;
+   position: relative;
+
+}
+
+.navbar ul li a {
+   text-decoration: none;
+   color: white;
+   font-family: Roboto, "sans-serif";
+   font-size: 1.2rem;
+}
+.btn-Inicio-index{
+   justify-content: right;
+    background: #4ae3b5;
+    padding: 0px 5px 0px 5px;
+    color: #fff;
+    text-decoration: none; 
+    border-radius:80px;
+ }
+
+.navbar ul li::before {
+   content: "";
+   height: 3px;
+   width: 0%;
+   background: rgb(145, 230, 106);
+   position: absolute;
+   left: 0;
+   bottom: 0px;
+   transition: 0.4s ease-out;
+}
+
+.navbar ul li:hover::before {
+   width: 95%;
+}
+
+@media (max-width:750px) {
+   .container {
+      width: 95%;
+      height:10vh;
+      background-image: linear-gradient(rgba(5, 10, 5, 0.80), rgba(74,227,181));
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+   }
+   
+   .navbar {
+      width: 100%;
+      margin: auto;
+      padding: 5px 0;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+   }
+   
+   .logo {
+      width: 130px;
+      cursor: pointer;
+      padding: 10px 10px 10px 10px;
+   }
+   .navbar ul li::before {
+      content: "";
+      height: 3px;
+      width: 0%;
+      background: rgb(230, 211, 106);
+      position: absolute;
+      left: 0;
+      bottom: 0px;
+      transition: 0.4s ease-out;
+   }
+   .btn-Inicio-index{
+      justify-content: right;
+       background: #2A5D67;
+       padding: 0px 10px 0px 10px;
+       color: #fff;
+       text-decoration: none; 
+       border-radius:80px;
+    }
+   
 }
 </style>
