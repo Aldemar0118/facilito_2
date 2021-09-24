a220da08e29b77e7ba1ff1e8e1e7ad489b5e1a8f
Aldemar0118 <79219065+Aldemar0118@users.noreply.github.com>
2021-09-23 20:41:05 -0500

    con vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,136 +1,50 @@
 <template>
-  <div id="app">
- <table class="container">
-        <tr>
-            <td class="navbar" >
-            <img src="./assets/Logo_blanco.png" class="logo" alt="Inicio" >
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/Logo_blanco.png"
+          transition="scale-transition"
+          width="100"
+        />
 
-        <ul>
-            
-            <li><a href="quienes_somos.html">Quienes Somos</a></li>
-            <li><router-link to="/login" class="btn-Inicio-index">Inicio de Secion</router-link></li>
-        </ul>
-    </td>
-    </tr>
-</table>
-<br>
-    <router-view/>
-  </div>
+      
+      </div>
+
+      <v-spacer>
+        
+      </v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
+<script>
 
-*{
-   margin: 0;
-   padding: 0;
-}
-li{
-   padding: 5px 2px 2px 4px;
-}
-.container {
-   width: 95%;
-   height:14vh;
-   background-image: linear-gradient(rgba(5, 10, 5, 0.80), rgba(42,93,103));
-   background-size: cover;
-   background-position: center;
-   background-repeat: no-repeat;
-}
+export default {
+  name: 'App',
 
-.navbar {
-   width: 100%;
-   margin: auto;
-   padding: 35px 0;
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-}
-
-.logo {
-   width: 130px;
-   cursor: pointer;
-   padding: 0px 10px 0px 10px;
-}
-
-.navbar ul li {
-   list-style: none;
-   display: inline-block;
-   margin: 0 20px;
-   position: relative;
-
-}
-
-.navbar ul li a {
-   text-decoration: none;
-   color: white;
-   font-family: Roboto, "sans-serif";
-   font-size: 1.2rem;
-}
-.btn-Inicio-index{
-   justify-content: right;
-    background: #4ae3b5;
-    padding: 0px 5px 0px 5px;
-    color: #fff;
-    text-decoration: none; 
-    border-radius:80px;
- }
-
-.navbar ul li::before {
-   content: "";
-   height: 3px;
-   width: 0%;
-   background: rgb(145, 230, 106);
-   position: absolute;
-   left: 0;
-   bottom: 0px;
-   transition: 0.4s ease-out;
-}
-
-.navbar ul li:hover::before {
-   width: 95%;
-}
-
-@media (max-width:750px) {
-   .container {
-      width: 95%;
-      height:10vh;
-      background-image: linear-gradient(rgba(5, 10, 5, 0.80), rgba(74,227,181));
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-   }
-   
-   .navbar {
-      width: 100%;
-      margin: auto;
-      padding: 5px 0;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-   }
-   
-   .logo {
-      width: 130px;
-      cursor: pointer;
-      padding: 10px 10px 10px 10px;
-   }
-   .navbar ul li::before {
-      content: "";
-      height: 3px;
-      width: 0%;
-      background: rgb(230, 211, 106);
-      position: absolute;
-      left: 0;
-      bottom: 0px;
-      transition: 0.4s ease-out;
-   }
-   .btn-Inicio-index{
-      justify-content: right;
-       background: #2A5D67;
-       padding: 0px 10px 0px 10px;
-       color: #fff;
-       text-decoration: none; 
-       border-radius:80px;
-    }
-   
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
